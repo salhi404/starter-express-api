b604e6471d6baf1823c6f1ce6e0c319581b3de3f
salhi <salhinfo404@gmail.com>
2023-01-20 03:22:44 +0100

    socket io cors fix 4

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var corsOptions = {
 };
 const io = require('socket.io')(http, {
   cors: {
-    origins: "https://elearnappsite.vercel.app"
+    origins:["https://elearnappsite.vercel.app"] 
   }
 });
 require("./app/routes/socket.routes")(io);
